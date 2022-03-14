9b768e8b774377ae9cb2b5098725eb3c90b1e967
spnikit <spnikit@gmail.com>
2022-03-15 00:09:14 +0300

    add notnull checks

--- src/com/spnikit/lesson3/Game.java
+++ src/com/spnikit/lesson3/Game.java
@@ -3,6 +3,7 @@ package com.spnikit.lesson3;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalInt;
 
@@ -13,6 +14,9 @@ class Game {
     private final IOManager manager = new IOManager();
 
     private Player getPlayer(PlayerNumber pn) {
+        Objects.requireNonNull(pn, "PlayerNumber object can't be null");
+
+
         Optional<String> playerName;
         do {
             manager.printToConsole("Введите имя игрока " + pn + " :");
@@ -25,6 +29,10 @@ class Game {
     }
 
     private int getCoordinate(Player playerToMove, String axis) {
+        Objects.requireNonNull(playerToMove, "Player object can't be null");
+        Objects.requireNonNull(axis, "axis object can't be null");
+
+
         OptionalInt coordinate;
         do {
             manager.printToConsole(playerToMove.getName() + ", введите координату " + axis);
@@ -37,6 +45,8 @@ class Game {
     }
 
     private void playOneRound(XmlGameWriter xmlw) {
+        Objects.requireNonNull(xmlw, "XmlGameWriter object can't be null");
+
         board = new GameBoard();
         board.printBoard();
 
@@ -51,7 +61,8 @@ class Game {
 
             try {
                 board.acceptMove(x, y, playerToMove.getToken());
-                xmlw.writeElementWithAttributes("Step",
+                xmlw.writeElementWithAttributes(
+                        "Step",
                         Map.of("num", numberOfMoves + "",
                                 "playerId", playerToMove.equals(player1) ? "1" : "2"),
                         "x: " + x, " y: " + y);
@@ -65,6 +76,7 @@ class Game {
 
             if (board.isWinner()) break;
             numberOfMoves++;
+
         } while (!board.isDraw());
 
         xmlw.writeStartElement("GameResult");
@@ -88,6 +100,8 @@ class Game {
     }
 
     public void play(XmlGameWriter xmlw) {
+        Objects.requireNonNull(xmlw, "XmlGameWriter object can't be null");
+
 
         do {
             manager.printToConsole("Привет, сейчас начнется игра в крестики-нолики");
@@ -146,8 +160,9 @@ class Game {
         return response.equalsIgnoreCase("да");
     }
 
-
     public void replay(Gameplay gameplay) {
+
+        Objects.requireNonNull(gameplay, "Gameplay object can't be null");
 
         do {
             manager.printToConsole("Привет, сейчас начнется игра в крестики-нолики");
@@ -171,8 +186,10 @@ class Game {
 
     }
 
-
     public void replayOneRound(Gameplay gameplay) {
+        Objects.requireNonNull(gameplay, "Gameplay object can't be null");
+
+
         var player1 = gameplay.getPlayer1();
         var player2 = gameplay.getPlayer2();
         var steps = gameplay.getSteps();
@@ -210,7 +227,6 @@ class Game {
             manager.printToConsole("Ничья!");
         }
     }
-
 
     private void sleep() {
         try {

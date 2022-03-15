a486500ead1106adbca1297a8c532880ac14c9d8
spnikit <spnikit@gmail.com>
2022-03-16 01:14:05 +0300

    add readme

--- src/com/spnikit/lesson3/Main.java
+++ src/com/spnikit/lesson3/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        // Создание объекта game и вызов метода play с передачей writer-a для записи результата в XML файл
 //            new Game().play(new XmlGameWriter("./test.xml"));
+
+        // парсинг XML файла с получением объекта Gameplay с последующей его передачей в игру для производства повтора
 
         new Game().replay(new XmlGameReader("./temp.xml").getGameplay());
 

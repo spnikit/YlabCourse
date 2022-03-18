ba204c3388b45da38972009d2ec573deba7617df
spnikit <spnikit@gmail.com>
2022-03-18 18:49:00 +0300

    fix readme

--- src/main/java/com/spnikit/lesson4/Main.java
+++ src/main/java/com/spnikit/lesson4/Main.java
@@ -1,5 +1,7 @@
 package com.spnikit.lesson4;
 
+import com.spnikit.lesson4.GameplayToFileMapper;
+
 import java.util.Optional;
 
 public class Main {
@@ -7,30 +9,30 @@ public class Main {
     public static void main(String[] args) {
 
 ////         Create new game
-//        var game = new Game();
+        var game = new Game();
+
+        //  Subscribe to the game listeners to construct gameplay object
+        var createGameplay = new GameplayConstructor(game);
+
+        // play the game
+        game.play();
+
+        // get constructed gameplay object
+        Gameplay gameplay = createGameplay.getGameplay();
 //
-//        //  Subscribe to the game listeners to construct gameplay object
-//        var createGameplay = new GameplayConstructor(game);
-//
-//        // play the game
-//        game.play();
-//
-//        // get constructed gameplay object
-//        Gameplay gameplay = createGameplay.getGameplay();
-//
-////         Write gameplay object to JSON
-//
-//        new GameplayToJsonMapper().writeToFile("./gameplay.json", gameplay);
+//         Write gameplay object to JSON
+        GameplayToFileMapper<Gameplay> mapper = new GameplayToJsonMapper();
+        mapper.writeToFile("./gameplay.json", gameplay);
 
 
-//         Game replay from JSON
-
-        // Create gameplay object from JSON
-        Optional<Gameplay> gameplay = new GameplayToJsonMapper().readFromFile("./gameplay.json");
-
-        // replay the game with gameplay object
-        gameplay.ifPresentOrElse(game -> new Game().replay(game),
-                () -> System.out.println("Gameplay object is not valid or not present"));
+////         Game replay from JSON
+//
+//        // Create gameplay object from JSON
+//        Optional<Gameplay> gameplay = new GameplayToJsonMapper().readFromFile("./gameplay.json");
+//
+//        // replay the game with gameplay object
+//        gameplay.ifPresentOrElse(game -> new Game().replay(game),
+//                () -> System.out.println("Gameplay object is not valid or not present"));
 
 
     }
